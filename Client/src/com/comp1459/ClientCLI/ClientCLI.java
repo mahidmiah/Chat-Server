c6d7fc5b04f96b6d152e725c0c98b494904bbb43
Lucas Gomes <lg2238c@gre.ac.uk>
2021-03-31 01:39:50 +0100

    Created CLI and ClientSocket handler classes
    Splitted the responsabilities of the original ChatClient into two classes: com.comp1459.ClientCLI.ClientCLI.java and com.comp1459.ClientSocket.java
    
    ClientCLI - Its sole purpose is to receive events from the clientSocket and show adequate messages to the user. Provides an interface to the user to interact with the client socket which will communicate with server
    ClientSocket - Multiple instances can be created. Its purpose is to create a client socket, communicate with the server and emitting events.

---
+++ Client/src/com/comp1459/ClientCLI/ClientCLI.java
@@ -0,0 +1,129 @@
+package com.comp1459.ClientCLI;
+import java.util.Scanner;
+
+import com.comp1459.ClientSocket;
+import com.comp1459.ColouredText;
+import com.comp1459.ClientSocket.ConnectionException;
+
+public class ClientCLI {
+	
+	private static Scanner systemInputScanner;
+	
+	public static class ClientSocketCLI extends ClientSocket {
+
+		public ClientSocketCLI(String serverIp, int serverPort) throws Exception {
+			super(serverIp, serverPort);
+		}
+		
+		/**
+		 * :::: ClientSocket and Client-Server communication events ::::
+		 * Override methods that handle Client-Server communication events
+		 */
+		
+		@Override
+		public void onServerRequestedTermination() {
+			System.out.println(ColouredText.ANSI_RED + ColouredText.ANSI_BOLD + "Disconnected from server!" + ColouredText.ANSI_RESET);
+            System.exit(0); //Exits the program.
+		}
+		
+		@Override
+		public void onServerNormalMessageReceived(String message) {
+			System.out.println(message);
+		}
+		
+		@Override
+		public void onServerReadError(Exception e) {
+			e.printStackTrace();
+			System.out.println(ColouredText.ANSI_RED + ColouredText.ANSI_BOLD + "[ChatClient] Server connection lost!" + ColouredText.ANSI_RESET);
+	        System.exit(0);
+			
+		}
+		// :::: ClientSocket and Client-Server communication events ::::
+		
+		
+		
+	}
+	
+	public static ClientSocketCLI clientSocketCLI;
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int port = -1;
+		String serverAddress = null;
+		
+		/**
+		 * Find the parameters(e.g --port, --server-address) and its values(e.g --port {value} --server-address {value})
+		 * that are in the CLI arguments
+		 * 
+		 */
+		for (int i = 0; i < args.length; i++) {
+			
+			String value = args[i];
+			if (value.equals("--port") && (i + 1) < args.length) {
+				port = Integer.parseInt(args[i+1]);
+			}
+			if (value.equals("--server-address") && (i + 1) < args.length) {
+				serverAddress = args[i+1];
+			}
+		}
+		
+		
+		
+		try {
+			clientSocketCLI = new ClientSocketCLI(serverAddress, port);
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+			if (e1 instanceof ClientSocket.VariableException) {
+				// Handles errors induced by incorrect types
+				switch(((ClientSocket.VariableException) e1).variableName) {
+					case "serverIp": {
+						System.out.println(ColouredText.ANSI_RED + ColouredText.ANSI_BOLD + "[ChatClient] Please provide a valid serverIp with the --server-address <address> argument." + ColouredText.ANSI_RESET);
+						System.exit(0);
+						break;
+					}
+					case "serverPort": {
+						System.out.println(ColouredText.ANSI_RED + ColouredText.ANSI_BOLD + "[ChatClient] Please provide a valid server port with the --port <port> argument." + ColouredText.ANSI_RESET);
+						System.exit(0);
+						break;
+					}
+					default: {
+						System.out.println(ColouredText.ANSI_RED + ColouredText.ANSI_BOLD + "[ChatClient] An error has occurred." + ColouredText.ANSI_RESET);
+						System.exit(0);
+						break;
+					}
+				}
+				
+			} else {
+				System.out.println(ColouredText.ANSI_RED + ColouredText.ANSI_BOLD + "[ChatClient] An error has occurred." + ColouredText.ANSI_RESET);
+				System.exit(0);
+			}
+		}
+		
+		// Connect to the server
+		try {
+			clientSocketCLI.runClient();
+			System.out.println(ColouredText.ANSI_YELLOW + ColouredText.ANSI_BOLD + "[ChatClient] Connected Successfully!" + ColouredText.ANSI_RESET);
+		} catch (ConnectionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println(ColouredText.ANSI_RED + ColouredText.ANSI_BOLD + "[ChatClient] Could not connect!" + ColouredText.ANSI_RESET);
+			System.exit(0);
+		}
+		
+		startInputReader();
+
+	}
+	
+	private static void startInputReader(){
+        // Everytime the user inputs a string into the console the input will be written to the server and handled on the server side.
+		systemInputScanner = new Scanner(System.in);
+		while (systemInputScanner.hasNextLine()) {
+   		 	clientSocketCLI.sendMessageToServer(systemInputScanner.nextLine());
+        }
+    }
+
+}
