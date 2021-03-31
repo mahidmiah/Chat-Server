b2a21249573c4f36168f39ff51e38d3403957468
Lucas Gomes <lg2238c@gre.ac.uk>
2021-04-01 00:53:21 +0100

    Do log on the server when there is a new coordinator

--- Server/src/com/comp1549/ServerMain.java
+++ Server/src/com/comp1549/ServerMain.java
@@ -186,6 +186,7 @@ public class ServerMain {
                         worker.isAdmin = true; //Sets the isAdmin boolean to true
                         adminExists = true; //Sets the adminExists boolean to true
                         worker.formattedUsername = ColouredText.ANSI_PURPLE + ColouredText.ANSI_BOLD + "[Coordinator] " + worker.formattedUsername; //Alters the clients username to include [Coordinator].
+                        System.out.println(String.format(Messages.NewCoordinator, worker.formattedUsername));
                         broadcastMessage(String.format(Messages.NewCoordinator, worker.formattedUsername)); //Broadcast to the server that this user is now the new Coordinator.
                         break;
                     }
